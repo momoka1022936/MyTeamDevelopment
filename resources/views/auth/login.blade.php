f3f5e9006cbd989cea4781f6c0e0fb3f3af4700c
camna-camna <camna08260@gmail.com>
2022-04-19 11:11:56 +0900

    ログイン機能CSS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <div class="container ">
     <div class="row justify-content-center col-xl-12 ">
-        <div class="col-md-8 border-bottom-1 pl-1">
+        <div class="col-md-6 border-bottom-1 pl-1">
             <div class="card border "> 
-                <div class="card-header text-body text-center border-bottom-1 bg-transparent">{{ __('家庭用在庫管理システム
+                <div class="card-header text-body text-center border-bottom-1 bg-gray-400">{{ __('家庭用在庫管理システム
                     ') }}</div>
 
                     <div class="card-body pb-2 bg-white">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
-                            <div class="form-group row   ">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('ID') }}</label>                               
-                                <div class="col-md-8 m-auto ">    
+                            <div class="form-group row mr-5">
+                                <label for="email" class="col-md-4 col-form-label text-md-right mr-auto">{{ __('ID') }}</label>                               
+                                <div class="col-md-7 m-auto pl-0 ">    
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror " name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                         @error('email')
@@ -25,9 +25,9 @@
 
                                 </div>        
                             </div>
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
-                                <div class="col-md-8 m-auto">
+                            <div class="form-group row mr-5">
+                                <label for="password" class="col-md-4 col-form-label text-md-right mr-auto ">{{ __('パスワード') }}</label>
+                                <div class="col-md-7 m-auto pl-0">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                     @error('password')
@@ -40,7 +40,7 @@
                             </div>   
 
                             <div class="form-group row mb-0 mx-auto bg-white pl-3">
-                                <div class="offset-md-4 py-3 px-4 col-lg-6 col-xl-6">
+                                <div class="offset-md-3 py-3 px-4 col-lg-6 col-xl-6">
                                     <button type="submit" class="btn btn-dark-none btn-primary">
                                         {{ __('ログイン') }}
                                     </button>
