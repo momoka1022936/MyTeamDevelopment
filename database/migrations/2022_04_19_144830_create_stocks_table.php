64d9264f7f7c5da84850a30f190ed9d4c935a366
momoka-toriizuka <momokatoriizuka@gmail.com>
2022-04-20 11:31:21 +0900

    カラムの追加

---
+++ database/migrations/2022_04_19_144830_create_stocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('stock_item_name', 100);
+            $table->unsignedDouble('quantity', 9, 3);
+            $table->date('stock_expiration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
