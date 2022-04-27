84dfb90da86ff7cd95b1efae57e08e46661f35c5
hatuyuki3569 <hirahirakaito3569@gmail.com>
2022-04-27 20:29:26 +0900

    コメントの追加

--- app/Http/Controllers/stocksControllers.php
+++ app/Http/Controllers/stocksControllers.php
@@ -5,22 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+use Carbon\Carbon;
 
 class stocksControllers extends Controller
 {
     //
 
-    
+    // stockテーブルのレコードを取得する処理
+    //selectは取得するカラムを指定することができる。この場合はすべて取得している。
     public function stocks()
     {
         $stocks = DB::select('select * from stocks');
         $data = ['stocks' => $stocks];
-        return view('stocks', $data);
+        return view('stocks.stocks', $data);
     }
+
+    //登録画面の表示
     public function stocksRegister()
     {
-        return view('stocksRegister');
+
+        return view('stocks.stocksRegister');
     }
+
+    // stocksのテーブルに登録するための機能
     public function stockCreate(Request $request){
 
         $stock = new Stock;
@@ -33,5 +41,6 @@ class stocksControllers extends Controller
 
         return redirect('/stocks');
     }
+    
 
 }
