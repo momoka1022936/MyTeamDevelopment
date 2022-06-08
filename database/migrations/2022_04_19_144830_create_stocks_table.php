bc35d3f8dddb18265b1b5bda6c55d3d75a196c90
momoka-toriizuka <momokatoriizuka@gmail.com>
2022-06-08 18:05:57 +0900

    在庫の日付をnullableに修正

--- database/migrations/2022_04_19_144830_create_stocks_table.php
+++ database/migrations/2022_04_19_144830_create_stocks_table.php
@@ -18,7 +18,7 @@ class CreateStocksTable extends Migration
             $table->bigInteger('user_id');
             $table->string('stock_item_name', 25);
             $table->unsignedDouble('quantity', 5, 1);
-            $table->date('stock_expiration');
+            $table->date('stock_expiration')->nullable();
             $table->timestamps();
         });
     }
